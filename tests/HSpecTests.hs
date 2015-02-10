a0a11c8cc112573173d34cc25b5b772375ed5028
Dominic Meiser <dmeiser@txcorp.com>
2015-02-10 16:58:39 -0700

    Use automatic test discovery with hspec.

--- tests/HSpecTests.hs
+++ tests/HSpecTests.hs
@@ -1,10 +1,1 @@
-module Main where
- 
-import HMbo
-import Test.Hspec
- 
-main :: IO ()
-main = hspec $ do
-  describe "Import HMbo module" $ do
-    it "Dummy test" $ do
-      (1 :: Int) `shouldBe` (1 ::Int)
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
