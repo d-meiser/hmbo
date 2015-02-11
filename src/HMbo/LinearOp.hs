f511e7e79b504cb56c1a03176d746e01376d5a8d
Dominic Meiser <dmeiser@txcorp.com>
2015-02-11 13:08:02 -0700

    Add stubs for operator algebra.

--- src/HMbo/LinearOp.hs
+++ src/HMbo/LinearOp.hs
@@ -3,7 +3,11 @@ module HMbo.LinearOp(
     getDim,
     Dim(unPositive),
     toDim,
-    fromDim
+    fromDim,
+    mul,
+    add,
+    scale,
+    kron
     ) where
 
 newtype Amplitude = Amplitude Double
@@ -29,6 +33,15 @@ getDim (Plus d _ _) = d
 getDim (KetBra d _) = d
 getDim (ScaledId d _) = d
 getDim (Null d) = d
+
+mul :: LinearOp -> LinearOp -> Maybe LinearOp
+mul = undefined
+add :: LinearOp -> LinearOp -> Maybe LinearOp
+add = undefined
+scale :: Amplitude -> LinearOp -> LinearOp
+scale = undefined
+kron :: LinearOp -> LinearOp -> LinearOp
+kron = undefined
 
 toDim :: Int -> Maybe Dim
 toDim d = if (d < 1) then Nothing else Just (Dim d)
