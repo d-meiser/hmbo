eeea626ebbe0c031dcbb332d1212ccdb63cb63a1
Dominic Meiser <dmeiser@txcorp.com>
2015-02-19 22:10:31 -0700

    Get rid of SparseMatrix module.

--- src/HMbo.hs
+++ src/HMbo.hs
@@ -1,8 +1,6 @@
 module HMbo (
-    module HMbo.SparseMatrix,
     module HMbo.LinearOp
     ) where
 
-import HMbo.SparseMatrix
 import HMbo.LinearOp
 
--- tests/HMbo/SparseMatrixSpec.hs
+++
@@ -1,9 +0,0 @@
-module HMbo.SparseMatrixSpec (spec) where
-
-import Test.Hspec
-import HMbo
-
-spec :: Spec
-spec = do
-  it "Zero Matrix has no entries" $ do
-    numEntries zeroMatrix `shouldBe` 0
